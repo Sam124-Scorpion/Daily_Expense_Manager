8014f3475fe927c705223486cc9832de60a8a775
Free-sam <soumikdas261@gmail.com>
2026-07-24 11:28:36 +0530

    hdh

--- Backend/backend/settings.py
+++ Backend/backend/settings.py
@@ -174,6 +174,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+
+
 if HAS_WHITENOISE:
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
